3befdc3bc777aded20448d1cd3ba6f2a65891a32
Eugene Sharygin <eush77@gmail.com>
2019-01-26 23:47:22 +0300

    [Arith] Limit to a single argument

--- clang/Arith/Arith.cpp
+++ clang/Arith/Arith.cpp
@@ -7,41 +7,35 @@
 
 namespace {
 
-void BM_Arith(benchmark::State &S, Node *Expr, int *Args) {
+int Params[] = {5, 7};
+ParamNode Param0{O_Param, 0};
+ParamNode Param1{O_Param, 1};
+IntNode One{O_Int, 1};
+IntNode Two{O_Int, 2};
+BinaryNode Inc{O_Add, reinterpret_cast<Node *>(&Param0),
+               reinterpret_cast<Node *>(&One)};
+BinaryNode Mul{O_Mul, reinterpret_cast<Node *>(&Param0),
+               reinterpret_cast<Node *>(&Param1)};
+BinaryNode Add{O_Add, reinterpret_cast<Node *>(&Inc),
+               reinterpret_cast<Node *>(&Mul)};
+BinaryNode Div{O_Div, reinterpret_cast<Node *>(&Add),
+               reinterpret_cast<Node *>(&Two)};
+
+void BM_Arith(benchmark::State &S) {
   for (auto _: S)
-    benchmark::DoNotOptimize(evalExpr(Expr, Args));
+    benchmark::DoNotOptimize(evalExpr(reinterpret_cast<Node *>(&Div), Params));
 }
 
-void BM_ArithMix(benchmark::State &S, const std::string &Name, Node *Expr,
-                 int *Args) {
-  Compiler C("Arith." + Name);
-  C.setFunction(mixEvalExpr(&C.getContext(), Expr));
+void BM_ArithMix(benchmark::State &S) {
+  Compiler C("Arith");
+  C.setFunction(mixEvalExpr(&C.getContext(), reinterpret_cast<Node *>(&Div)));
   auto *F = reinterpret_cast<int (*)(int *)>(C.compile());
 
   for (auto _ : S)
-    benchmark::DoNotOptimize(F(Args));
+    benchmark::DoNotOptimize(F(Params));
 }
 
-static int Params[] = {5, 7};
-static ParamNode Param0{O_Param, 0};
-static ParamNode Param1{O_Param, 1};
-static IntNode One{O_Int, 1};
-static IntNode Two{O_Int, 2};
-static BinaryNode Inc{O_Add, reinterpret_cast<Node *>(&Param0),
-                      reinterpret_cast<Node *>(&One)};
-static BinaryNode Mul{O_Mul, reinterpret_cast<Node *>(&Param0),
-                       reinterpret_cast<Node *>(&Param1)};
-static BinaryNode Add{O_Add, reinterpret_cast<Node *>(&Inc),
-                      reinterpret_cast<Node *>(&Mul)};
-static BinaryNode Div{O_Div, reinterpret_cast<Node *>(&Add),
-                      reinterpret_cast<Node *>(&Two)};
-
-BENCHMARK_CAPTURE(BM_Arith, Inc, reinterpret_cast<Node *>(&Inc), Params);
-BENCHMARK_CAPTURE(BM_ArithMix, Inc, "Inc", reinterpret_cast<Node *>(&Inc),
-                  Params);
-
-BENCHMARK_CAPTURE(BM_Arith, Expr, reinterpret_cast<Node *>(&Div), Params);
-BENCHMARK_CAPTURE(BM_ArithMix, Expr, "Expr", reinterpret_cast<Node *>(&Div),
-                  Params);
+BENCHMARK(BM_Arith);
+BENCHMARK(BM_ArithMix);
 
 } // namespace
